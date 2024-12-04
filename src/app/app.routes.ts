08bd8b8e48cffd1d9f363e993976e48d2eb3d325
cherryleh <cherryle@hawaii.edu>
2024-12-03 18:03:22 -1000

    Adjust home routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent }, // Adjust if you have a home component
+    { path: '', component: HomeComponent }, // Adjust if you have a home component
   { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page
 ];
