3c3d27592ff27e6f496f720430785855e2498570
cherryleh <cherryle@hawaii.edu>
2024-12-03 17:55:58 -1000

    Added home and about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }, // Adjust if you have a home component
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '/home' } // Wildcard route for a 404 page
+];
